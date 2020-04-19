8c26ca2a32c9a6482391fa0b1f0513ba6a7bd9b6
DING Wei <SX5533@engie.com>
2020-04-19 22:31:33 +0200

    add test

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 from setuptools import setup, find_packages
-
 import liu_ml_pkg
 
 setup(
-
     name = 'liu_ml_pkg',
     version = liu_ml_pkg.__version__, 
     author = 'liu_sulan',
     packages = find_packages(),
     include_package_data=True,
     install_requires=['pandas'],
-    tests_require=['pandas','liu_ml_pkg'],
-    
+    tests_require=['pandas','liu_ml_pkg']
 )
